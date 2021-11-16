249906cd5fd62104022309f38a9fd8b21c6dcedb
ngocthinh <ngocthinh1126@gmail.com>
2021-11-16 21:51:15 +0700

    overlay camera

---
+++ app/src/main/java/com/phungtsm/test/fatboyOCR/Utils.java
@@ -0,0 +1,15 @@
+package com.phungtsm.test.fatboyOCR;
+
+import android.graphics.Bitmap;
+import android.util.Base64;
+
+import java.io.ByteArrayOutputStream;
+
+public class Utils {
+
+    public static String convertToBase64(Bitmap bitmap) {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 80, outputStream);
+        return Base64.encodeToString(outputStream.toByteArray(), Base64.DEFAULT);
+    }
+}

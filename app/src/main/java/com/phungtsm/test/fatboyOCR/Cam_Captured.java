340403e0e45c29d022408314ac53469f825ad5ee
ngocthinh <ngocthinh1126@gmail.com>
2021-11-27 21:36:58 +0700

    27/11

--- app/src/main/java/com/phungtsm/test/fatboyOCR/Cam_Captured.java
+++ app/src/main/java/com/phungtsm/test/fatboyOCR/Cam_Captured.java
@@ -1,23 +1,49 @@
 package com.phungtsm.test.fatboyOCR;
 
+import static com.phungtsm.test.fatboyOCR.Camera.getBitmapFromMemCache;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-public class Cam_Captured extends AppCompatActivity {
+public class Cam_Captured extends AppCompatActivity implements View.OnClickListener {
+    Button btnTiepTucMS;
     ImageView imagePreview;
+    TextView txtChupLai;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam_captured);
 
         imagePreview = findViewById(R.id.imagePreview);
-
+        btnTiepTucMS = findViewById(R.id.btnTiepTucMS);
+        txtChupLai = findViewById(R.id.txtChupLai);
         Intent intent = getIntent();
-        Bitmap bitmap =(Bitmap) intent.getParcelableExtra("pic");
-        imagePreview.setImageBitmap(bitmap);
+        Bitmap pic = getBitmapFromMemCache(intent.getStringExtra("mattruoc"));
+
+        imagePreview.setImageBitmap(pic);
+        txtChupLai.setOnClickListener(this);
+        btnTiepTucMS.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.txtChupLai:
+                finish();
+                break;
+            case R.id.btnTiepTucMS:
+                Intent myIntent = new Intent(this, Camera.class);
+                myIntent.putExtra("matsau","CHỤP MẶT SAU");
+                startActivity(myIntent);
+                break;
+        }
+
     }
 }
--- app/src/main/java/com/phungtsm/test/fatboyOCR/Camera.java
+++ app/src/main/java/com/phungtsm/test/fatboyOCR/Camera.java
@@ -8,11 +8,13 @@ import android.graphics.Canvas;
 import android.graphics.Path;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.LruCache;
 
 import com.camerakit.CameraKitView;
 
@@ -23,11 +25,19 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 
 public class Camera extends AppCompatActivity {
+    private static LruCache<String, Bitmap> mMemoryCache;
+
+
     private CameraKitView cameraKitView;
     private ImageView photoButtonCap;
     private FrameLayout cameraLayout;
     private ViewFinderView viewFinderView;
     private TextView txtTitle;
+    private static TextView txtDir;
+    private ImageView photoButtonClose;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,41 +50,65 @@ public class Camera extends AppCompatActivity {
         viewFinderView = new ViewFinderView(getApplicationContext());
         cameraLayout.addView(viewFinderView, 1);
         txtTitle = findViewById(R.id.txtTitle);
+        txtDir = findViewById(R.id.txtDir);
+        photoButtonClose = findViewById(R.id.photoButtonClose);
+        photoButtonClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
-        if(MainActivity.Flag == 1 || MainActivity.Flag == 2){
+
+        if(getIntent().getExtras()!=null){
+            txtDir.setText(getIntent().getStringExtra("matsau"));
+        }
+
+        // Get max available VM memory, exceeding this amount will throw an
+        // OutOfMemory exception. Stored in kilobytes as LruCache takes an
+        // int in its constructor.
+        final int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);
+
+        // Use 1/8th of the available memory for this memory cache.
+        final int cacheSize = maxMemory / 8;
+
+        mMemoryCache = new LruCache<String, Bitmap>(cacheSize) {
+            @Override
+            protected int sizeOf(String key, Bitmap bitmap) {
+                // The cache size will be measured in kilobytes rather than
+                // number of items.
+                return bitmap.getByteCount() / 1024;
+            }
+        };
+
+        if (MainActivity.Flag == 1 || MainActivity.Flag == 2) {
             txtTitle.setText("Chứng minh thư/Thẻ căn cước");
-        }else if(MainActivity.Flag == 3){
+        } else if (MainActivity.Flag == 3) {
             txtTitle.setText("Hộ chiếu");
         }
 
         photoButtonCap.setOnClickListener(v -> {
             cameraKitView.captureImage((cameraKitView, capturedImage) -> {
                 File savedPhoto = new File(getExternalFilesDir(null), "/camerakittest.jpg");
-                Log.e("Status", "trang thai: "+savedPhoto.getPath());
+                Log.e("Status", "trang thai: " + savedPhoto.getPath());
                 try {
                     Bitmap bitmap = BitmapFactory.decodeByteArray(capturedImage, 0, capturedImage.length);
 
-                    Log.d("onCreate: ","left:"+viewFinderView.getFrameRect().left+"-TOP:"+viewFinderView.getFrameRect().top+"-Right:"+
-                            viewFinderView.getFrameRect().right +"-Bottom"+
+                    Log.d("onCreate: ", "left:" + viewFinderView.getFrameRect().left + "-TOP:" + viewFinderView.getFrameRect().top + "-Right:" +
+                            viewFinderView.getFrameRect().right + "-Bottom" +
                             viewFinderView.getFrameRect().bottom +
-                            "bitmapWidth:"+bitmap.getWidth()+
-                            "bitmapHegiht:"+bitmap.getHeight()+
-                            "width"+viewFinderView.getWidth()+
-                            "height"+viewFinderView.getHeight()
-
+                            "bitmapWidth:" + bitmap.getWidth() +
+                            "bitmapHegiht:" + bitmap.getHeight() +
+                            "width" + viewFinderView.getWidth() +
+                            "height" + viewFinderView.getHeight()
                     );
 
                     Bitmap croppedBitmap = Bitmap.createBitmap(bitmap,
                             viewFinderView.getFrameRect().left,
                             viewFinderView.getFrameRect().top,
-                            (viewFinderView.getFrameRect().right - viewFinderView.getFrameRect().left),
-                            (viewFinderView.getFrameRect().bottom - viewFinderView.getFrameRect().top));
-//                    Bitmap croppedBitmap = Bitmap.createBitmap(bitmap,
-//                            (int)(viewFinderView.getFrameRect().left/1.3),
-//                            (int)(viewFinderView.getFrameRect().top/1.5),
-//                            (int)(viewFinderView.getFrameRect().right/1.3 - viewFinderView.getFrameRect().left/1.3),
-//                            (int)(viewFinderView.getFrameRect().bottom/1.58 - viewFinderView.getFrameRect().top/1.58)
-//                            );
+                            (viewFinderView.getFrameRect().right - viewFinderView.getFrameRect().left-100),
+                            (viewFinderView.getFrameRect().bottom - viewFinderView.getFrameRect().top)-100);
+                    addBitmapToMemoryCache("mattruoc",croppedBitmap);
 
                     FileOutputStream outputStream = new FileOutputStream(savedPhoto);
                     ByteArrayOutputStream stream = new ByteArrayOutputStream();
@@ -84,26 +118,28 @@ public class Camera extends AppCompatActivity {
                     outputStream.close();
 
                     String croppedBitmapBase64 = Utils.convertToBase64(croppedBitmap);
-                    Log.d("onCreate: ",croppedBitmapBase64);
+                    Log.d("onCreate: ", croppedBitmapBase64);
                     Intent myIntent = new Intent(this, Cam_Captured.class);
-                        myIntent.putExtra("pic", croppedBitmap);
-                        startActivity(myIntent);
+                    myIntent.putExtra("mattruoc", "mattruoc");
+                    startActivity(myIntent);
 
-//                    FileOutputStream outputStream = new FileOutputStream(savedPhoto);
-//                    ByteArrayOutputStream stream = new ByteArrayOutputStream();
-//                    bitmap.compress(Bitmap.CompressFormat.JPEG, 100, stream);
-//                    byte[] byteArray = stream.toByteArray();
-//                    outputStream.write(byteArray);
-//                    outputStream.close();
-//                        Intent myIntent = new Intent(MainActivity.this, MainActivity2.class);
-//                        myIntent.putExtra("basecode", savedPhoto.getPath());
-//                        startActivity(myIntent);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
             });
         });
     }
+
+    public void addBitmapToMemoryCache(String key, Bitmap bitmap) {
+        if (getBitmapFromMemCache(key) == null) {
+            mMemoryCache.put(key, bitmap);
+        }
+    }
+
+    public static Bitmap getBitmapFromMemCache(String key) {
+        return mMemoryCache.get(key);
+    }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -139,8 +175,7 @@ public class Camera extends AppCompatActivity {
             OutputStreamWriter outputStreamWriter = new OutputStreamWriter(context.openFileOutput("config.txt", Context.MODE_PRIVATE));
             outputStreamWriter.write(data);
             outputStreamWriter.close();
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             Log.e("Exception", "File write failed: " + e.toString());
         }
     }
